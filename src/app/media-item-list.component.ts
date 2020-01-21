022cf0cc5e1f17791e4cc47b2f36001dd0fb1e02
Ericcentrik <enaiman1@gmail.com>
2020-01-20 20:39:22 -0500

    add a catergory-list component and adde some styling

--- src/app/media-item-list.component.ts
+++ src/app/media-item-list.component.ts
@@ -11,20 +11,19 @@ export class MediaItemListComponent implements OnInit {
   medium = '';
   mediaItems: MediaItem[];
 
-  constructor(private mediaItemService: MediaItemService, 
-    private activatedRoute: ActivatedRoute) {
-    
-  }
+  constructor(
+    private mediaItemService: MediaItemService,
+    private activatedRoute: ActivatedRoute) {}
 
   ngOnInit() {
     this.activatedRoute.paramMap
-    .subscribe(paramMap =>{
-      let medium = paramMap.get('medium');
-      if (medium.toLowerCase() === 'all') {
-        medium = '';
-      }
-      this.getMediaItems(medium);
-    });
+      .subscribe(paramMap => {
+        let medium = paramMap.get('medium');
+        if (medium.toLowerCase() === 'all') {
+          medium = '';
+        }
+        this.getMediaItems(medium);
+      });
   }
 
   onMediaItemDelete(mediaItem: MediaItem) {
